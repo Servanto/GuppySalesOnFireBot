e60a71a47ebb6cb0289adc18e6c28cf4373f7e7c
Servanto <>
2022-03-06 16:21:49 +0100

    removes python

--- guppyFireSaleBot.py
+++
@@ -1,7 +0,0 @@
-from opensea import OpenseaAPI
-
-# If I have an API key, use this method here!
-# api = OpenseaAPI(apikey="YOUR APIKEY")
-api = OpenseaAPI()
-
-guppyCollection = api.collection(collection_slug='guppygang')
